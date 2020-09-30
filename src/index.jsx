da3d5719cb204f74c8205bc01bbaea426ebb65a7
Luca Allegro <luca.allegro@zextras.com>
2020-09-30 10:28:33 +0200

    Refactored and added SAML handlig according to docs

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,49 @@
-import React, { useState } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { extendTheme, SnackbarManager, ThemeProvider } from '@zextras/zapp-ui';
 
-import LoginView from './components/login-view';
 import '../i18n/i18n.config';
 import './index.css';
+import { getLoginSupported } from './services/login-page-services';
+import { useTranslation } from 'react-i18next';
 
-function App() {
+const Loader = lazy(function LoaderFn () {
+	const domain = new URLSearchParams(window.location.search).get('domain');
+	return getLoginSupported(domain)
+		.then(({ maxApiVersion }) => {
+			switch (maxApiVersion) {
+				case 1:
+					return import(/* webpackChunkName: "v1" */ './components-v1/page-layout');
+			}
+		})
+		.catch(() => {
+			console.error('The server is not responding. Please contact your server administrator');
+			alert('The server is not responding. Please contact your server administrator');
+		});
+});
+
+function App () {
 	const [theme, setTheme] = useState({});
+	const { t } = useTranslation();
 
 	return (
-		<>
-			<ThemeProvider theme={extendTheme(theme)}>
-				<SnackbarManager>
+		<ThemeProvider theme={extendTheme(theme)}>
+			<SnackbarManager>
+				<Suspense fallback={
+					<div></div>
+				}>
 					<Router>
 						<Switch>
-							<Route path="/">
-								<LoginView theme={theme} setTheme={setTheme} />
-							</Route>
+							<Loader theme={theme} setTheme={setTheme}/>
 						</Switch>
 					</Router>
-				</SnackbarManager>
-			</ThemeProvider>
-		</>
+				</Suspense>
+			</SnackbarManager>
+		</ThemeProvider>
 	);
 }
 
-render(<App />, document.getElementById('app'));
+render(
+	<App/>
+	, document.getElementById('app'));
