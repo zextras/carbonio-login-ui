f0b0d76aa39531e37e58dc07fd343b0beb8220bf
Matteo Dal Zovo <matteo.dalzovo@zextras.com>
2021-09-02 17:41:12 +0200

    updated route for carbonio shell

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const MAX_SUPPORTED_VERSION = 2; // to keep updated adding new versions
-export const IRIS_URL = '/static/iris/zextras-zapp-shell/current/';
+export const IRIS_URL = '/static/iris/carbonio-shell/current/';
 export const IRIS_CHECK_URL = '/static/iris/components.json';
 export const DEFAULT_UI = 'iris';
