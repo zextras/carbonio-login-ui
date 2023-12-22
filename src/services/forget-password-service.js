1d3a1f7a9efeab6071bf097e65ec820c41070e1a
Vijay Godhasara <101629244+zextras-vijay@users.noreply.github.com>
2023-12-22 15:38:51 +0530

    Ac 876 forget password (#68)
    * fix: ac-876: forget password
    
    * fix: lint issue
    
    * fix: add cookie and change trans

---
+++ src/services/forget-password-service.js
@@ -0,0 +1,69 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+export function getRecoveryAccount(username) {
+	return fetch('/service/soap/RecoverAccountRequest', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		credentials: 'omit',
+		body: JSON.stringify({
+			Body: {
+				RecoverAccountRequest: {
+					_jsns: 'urn:zimbraMail',
+					op: 'getRecoveryAccount',
+					email: username,
+					channel: 'email'
+				}
+			}
+		})
+	});
+}
+
+export function getSendRecoveryCode(username) {
+	return fetch('/service/soap/RecoverAccountRequest', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		credentials: 'omit',
+		body: JSON.stringify({
+			Body: {
+				RecoverAccountRequest: {
+					_jsns: 'urn:zimbraMail',
+					op: 'sendRecoveryCode',
+					email: username,
+					channel: 'email'
+				}
+			}
+		})
+	});
+}
+
+export function validateRecoveryCode(username, recoveryCode) {
+	return fetch('/service/soap/AuthRequest', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		credentials: 'omit',
+		body: JSON.stringify({
+			Body: {
+				AuthRequest: {
+					_jsns: 'urn:zimbraAccount',
+					csrfTokenSecured: '1',
+					account: {
+						by: 'name',
+						_content: username
+					},
+					recoveryCode: {
+						_content: recoveryCode
+					}
+				}
+			}
+		})
+	});
+}
