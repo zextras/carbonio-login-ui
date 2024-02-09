783e65273111d548239f47cc69f58e71c50eb7ee
Divyang Dodiya <divyang@dolphinwebsolution.com>
2024-02-09 18:10:36 +0530

    fix: ac-896: ui issue fixed for login form

--- src/components-v1/credentials-form.jsx
+++ src/components-v1/credentials-form.jsx
@@ -79,64 +79,62 @@ export default function CredentialsForm({
 	);
 
 	return (
-		<Row>
-			<form style={{ width: '100%' }} data-testid="credentials-form">
-				<Row padding={{ bottom: 'large' }}>
-					<Input
-						defaultValue={username}
-						disabled={disableInputs}
-						data-testid="username"
-						onChange={(e) => setUsername(e.target.value)}
-						hasError={!!authError}
-						autocomplete="username"
-						label={t('username', 'Username')}
-						backgroundColor="gray5"
-					/>
+		<form style={{ width: '100%' }} data-testid="credentials-form">
+			<Row padding={{ bottom: 'large' }}>
+				<Input
+					defaultValue={username}
+					disabled={disableInputs}
+					data-testid="username"
+					onChange={(e) => setUsername(e.target.value)}
+					hasError={!!authError}
+					autocomplete="username"
+					label={t('username', 'Username')}
+					backgroundColor="gray5"
+				/>
+			</Row>
+			<Row padding={{ bottom: 'small' }}>
+				<PasswordInput
+					defaultValue={password}
+					disabled={disableInputs}
+					data-testid="password"
+					onChange={(e) => setPassword(e.target.value)}
+					hasError={!!authError}
+					autocomplete="password"
+					label={t('password', 'Password')}
+					backgroundColor="gray5"
+				/>
+			</Row>
+			<Text color="error" size="medium" overflow="break-word">
+				{authError || <br />}
+			</Text>
+			<Row
+				orientation="vertical"
+				crossAlignment="flex-start"
+				padding={{ bottom: 'large', top: 'small' }}
+			>
+				<Button
+					loading={loading}
+					data-testid="login"
+					onClick={submitUserPassword}
+					disabled={disableInputs}
+					label={t('login', 'Login')}
+					width="fill"
+				/>
+			</Row>
+			<Row mainAlignment="flex-end" padding={{ bottom: 'extralarge' }}>
+				{samlButton}
+			</Row>
+			{carbonioFeatureResetPasswordEnabled !== false && (
+				<Row mainAlignment="flex-start" crossAlignment="flex-start">
+					<Text
+						onClick={clickForgetPassword}
+						color="primary"
+						style={{ textDecorationLine: 'underline', cursor: 'pointer' }}
+					>
+						{t('forget_password', 'Forget Password?')}
+					</Text>
 				</Row>
-				<Row padding={{ bottom: 'small' }}>
-					<PasswordInput
-						defaultValue={password}
-						disabled={disableInputs}
-						data-testid="password"
-						onChange={(e) => setPassword(e.target.value)}
-						hasError={!!authError}
-						autocomplete="password"
-						label={t('password', 'Password')}
-						backgroundColor="gray5"
-					/>
-				</Row>
-				<Text color="error" size="medium" overflow="break-word">
-					{authError || <br />}
-				</Text>
-				<Row
-					orientation="vertical"
-					crossAlignment="flex-start"
-					padding={{ bottom: 'large', top: 'small' }}
-				>
-					<Button
-						loading={loading}
-						data-testid="login"
-						onClick={submitUserPassword}
-						disabled={disableInputs}
-						label={t('login', 'Login')}
-						width="fill"
-					/>
-				</Row>
-				<Row mainAlignment="flex-end" padding={{ bottom: 'extralarge' }}>
-					{samlButton}
-				</Row>
-				{carbonioFeatureResetPasswordEnabled !== false && (
-					<Row mainAlignment="flex-start" crossAlignment="flex-start">
-						<Text
-							onClick={clickForgetPassword}
-							color="primary"
-							style={{ textDecorationLine: 'underline', cursor: 'pointer' }}
-						>
-							{t('forget_password', 'Forget Password?')}
-						</Text>
-					</Row>
-				)}
-			</form>
-		</Row>
+			)}
+		</form>
 	);
 }
