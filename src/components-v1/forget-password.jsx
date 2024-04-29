548cd4eeeb8620b8da84c8595bc2eeb368a9905e
Vijay Godhasara <101629244+zextras-vijay@users.noreply.github.com>
2024-04-29 14:53:10 +0530

    Revert "Ac 1009 forget password (#92)"
    This reverts commit c6909de79890c612fd0ad752cdf207b2510162ef.

--- src/components-v1/forget-password.jsx
+++ src/components-v1/forget-password.jsx
@@ -10,34 +10,23 @@ import {
 	Input,
 	Button,
 	Container,
-	SnackbarManagerContext,
-	Select,
-	Checkbox
+	SnackbarManagerContext
 } from '@zextras/carbonio-design-system';
 import { Trans, useTranslation } from 'react-i18next';
-import { map } from 'lodash';
 import {
 	getRecoveryAccount,
-	getSendRecoveryToken,
-	validateRecoveryToken
+	getSendRecoveryCode,
+	validateRecoveryCode
 } from '../services/forget-password-service';
+import { ZM_AUTH_TOKEN } from '../constants';
 import { setCookie } from '../utils';
-import {
-	FEATURE_RESET_PASSWORD_DISABLED,
-	ZM_AUTH_TOKEN,
-	CONTACT_ADMIN,
-	RECOVERY_TOKEN,
-	CARBONIO
-} from '../constants';
-import { postV2Login, submitOtp } from '../services/v2-service';
 
 const urlParams = new URLSearchParams(window.location.search);
 
 const state = {
 	retrieveRecoveryEmail: 'retrieve-recovery-email',
-	sendRecoveryToken: 'send-recovery-token',
-	validateRecoveryToken: 'validate-recovery-token',
-	submitOtpCode: 'submit-otp-code',
+	sendRecoveryCode: 'send-recovery-code',
+	validateRecoveryCode: 'validate-recovery-code',
 	proceedToLoginScreen: 'proceed-to-login-screen'
 };
 
@@ -45,53 +34,13 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 	const [t] = useTranslation();
 	const [username, setUsername] = useState(urlParams.get('username') || '');
 	const [progress, setProgress] = useState(state.retrieveRecoveryEmail);
-	const [token, setToken] = useState('');
+	const [code, setCode] = useState('');
 	const [recoveryEmail, setRecoveryEmail] = useState('');
-	const [recoveryEmailError, setRecoveryEmailError] = useState('');
-	const [sendTokenError, setSendTokenError] = useState('');
-	const [validationTokenError, setValidationTokenError] = useState('');
+	const [showRecoveryEmailError, setShowRecoveryEmailError] = useState(false);
+	const [showSendCodeError, setShowSendCodeError] = useState(false);
+	const [showValidationCodeError, setShowValidationCodeError] = useState(false);
 	const [recoveryAttemptsLeft, setRecoveryAttemptsLeft] = useState();
-	const [validationOTPError, setValidationOTPError] = useState('');
 	const createSnackbar = useContext(SnackbarManagerContext);
-	const [otpList, setOtpList] = useState([]);
-	const [otpId, setOtpId] = useState('');
-	const [otp, setOtp] = useState('');
-	const [trustDevice, setTrustDevice] = useState(false);
-	const toggleTrustDevice = useCallback(() => setTrustDevice((v) => !v), [setTrustDevice]);
-	const onChangeOtp = useCallback(
-		(ev) => {
-			setOtp(ev.target.value);
-		},
-		[setOtp]
-	);
-
-	const handleAccountRecoveryError = useCallback(
-		(errorCode) => {
-			switch (errorCode) {
-				case FEATURE_RESET_PASSWORD_DISABLED:
-					setRecoveryEmailError(
-						t('password_Reset_feature_disabled', 'Password reset feature is disabled.')
-					);
-					break;
-
-				case CONTACT_ADMIN:
-					setRecoveryEmailError(
-						t(
-							'account_recovery_not_found_or_not_verified',
-							'Account Recovery is either not found or not verified. Please contact your administrator.'
-						)
-					);
-					break;
-
-				default:
-					setRecoveryEmailError(
-						t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-					);
-					break;
-			}
-		},
-		[t]
-	);
 
 	const onRetrieveRecoveryEmail = useCallback(() => {
 		getRecoveryAccount(username)
@@ -100,228 +49,79 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 				try {
 					payload = await res.json();
 				} catch (err) {
-					payload = res;
+					payload = await res;
 				}
-				let email;
-				switch (res.status) {
-					case 200:
-						email = payload?.Body?.RecoverAccountResponse?.recoveryAccount;
-						setRecoveryEmail(email);
-						setProgress(state.sendRecoveryToken);
-						break;
-					case 401:
-					case 500:
-						handleAccountRecoveryError(payload?.Body?.Fault?.Detail?.Error?.Code);
-						break;
-					case 502:
-						setRecoveryEmailError(
-							t('server_unreachable', 'Error 502: Service Unreachable - Retry later.')
-						);
-						break;
-					default:
-						setRecoveryEmailError(
-							t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-						);
-						break;
+				if (res.status === 200) {
+					const email = payload?.Body?.RecoverAccountResponse?.recoveryAccount;
+					setRecoveryEmail(email);
+					setProgress(state.sendRecoveryCode);
+				} else {
+					setShowRecoveryEmailError(true);
 				}
 			})
 			.catch((err) => {
-				setRecoveryEmailError(
-					t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-				);
+				setShowRecoveryEmailError(true);
 			});
-	}, [handleAccountRecoveryError, t, username]);
+	}, [username]);
 
-	const onSendRecoveryToken = useCallback(() => {
-		getSendRecoveryToken(username)
+	const onSendRecoveryCode = useCallback(() => {
+		getSendRecoveryCode(username)
 			.then(async (res) => {
 				let payload;
 				try {
 					payload = await res.json();
 				} catch (err) {
-					payload = res;
+					payload = await res;
 				}
-				let rAttemptLeft;
-				switch (res.status) {
-					case 200:
-						rAttemptLeft = payload?.Body?.RecoverAccountResponse?.recoveryAttemptsLeft;
-						setRecoveryAttemptsLeft(rAttemptLeft);
-						createSnackbar({
-							key: 'success',
-							type: 'success',
-							label: t(
-								'recovery_code_send_success',
-								'The Recovery code has been send successfully'
-							),
-							autoHideTimeout: 3000,
-							hideButton: true,
-							replace: true
-						});
-						setProgress(state.validateRecoveryToken);
-						break;
-					case 502:
-						setSendTokenError(
-							t('server_unreachable', 'Error 502: Service Unreachable - Retry later.')
-						);
-						break;
-					default:
-						setSendTokenError(
-							t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-						);
-						break;
+				if (res.status === 200) {
+					const rAttemptLeft = payload?.Body?.RecoverAccountResponse?.recoveryAttemptsLeft;
+					setRecoveryAttemptsLeft(rAttemptLeft);
+					createSnackbar({
+						key: 'success',
+						type: 'success',
+						label: t('recovery_code_send_success', 'The Recovery code has been send successfully'),
+						autoHideTimeout: 3000,
+						hideButton: true,
+						replace: true
+					});
+					setProgress(state.validateRecoveryCode);
+				} else {
+					setShowSendCodeError(true);
 				}
 			})
 			.catch((err) => {
-				setSendTokenError(
-					t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-				);
+				setShowSendCodeError(true);
 			});
 	}, [createSnackbar, t, username]);
 
-	const validateCommunityToken = useCallback(() => {
-		validateRecoveryToken(username, token)
+	const onValidateCode = useCallback(() => {
+		validateRecoveryCode(username, code)
 			.then(async (res) => {
 				let payload;
 				try {
 					payload = await res.json();
 				} catch (err) {
-					payload = res;
+					payload = await res;
 				}
-				let authTokenArr;
-				let authToken;
-				switch (res.status) {
-					case 200:
-						authTokenArr = payload?.Body?.AuthResponse?.authToken;
-						authToken =
-							authTokenArr && authTokenArr.length > 0 ? authTokenArr[0]._content : undefined;
-						if (authToken) {
-							setCookie(ZM_AUTH_TOKEN, authToken);
-						}
-						setProgress(state.proceedToLoginScreen);
-						break;
-					case 401:
-					case 500:
-						setValidationTokenError(
-							t('authentication_error', 'Authentication Error - Retry later.')
-						);
-						break;
-					case 502:
-						setValidationTokenError(
-							t('server_unreachable', 'Error 502: Service Unreachable - Retry later.')
-						);
-						break;
-					default:
-						setValidationTokenError(
-							t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-						);
-						break;
-				}
-			})
-			.catch((err) => {
-				setValidationTokenError(
-					t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-				);
-			});
-	}, [t, token, username]);
-
-	const validateCarbonioToken = useCallback(() => {
-		postV2Login(RECOVERY_TOKEN, username, token)
-			.then(async (res) => {
-				let payload;
-				try {
-					payload = await res.json();
-				} catch (err) {
-					payload = res;
-				}
-				switch (res.status) {
-					case 200:
-						if (payload?.['2FA'] === true) {
-							setOtpList(
-								map(payload?.otp ?? [], (obj) => ({
-									label: obj.label,
-									value: obj.id
-								}))
-							);
-							setOtpId(payload?.otp?.[0].id);
-							setProgress(state.submitOtpCode);
-						} else {
-							setProgress(state.proceedToLoginScreen);
-						}
-						break;
-					case 401:
-					case 500:
-						setValidationTokenError(
-							t('authentication_error', 'Authentication Error - Retry later.')
-						);
-						break;
-					case 502:
-						setValidationTokenError(
-							t('server_unreachable', 'Error 502: Service Unreachable - Retry later.')
-						);
-						break;
-					default:
-						setValidationTokenError(
-							t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-						);
-						break;
-				}
-			})
-			.catch((err) => {
-				setValidationTokenError(
-					t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-				);
-			});
-	}, [t, token, username]);
-
-	const onValidateToken = useCallback(() => {
-		if (configuration?.systemType === CARBONIO) {
-			validateCarbonioToken();
-		} else {
-			validateCommunityToken();
-		}
-	}, [configuration, validateCarbonioToken, validateCommunityToken]);
-
-	const submitOtpCb = useCallback(
-		(e) => {
-			submitOtp(otpId, otp, trustDevice)
-				.then((res) => {
-					switch (res.status) {
-						case 200:
-							setProgress(state.proceedToLoginScreen);
-							break;
-						case 400:
-						case 401:
-						case 500:
-							setValidationOTPError(
-								t('authentication_error', 'Authentication Error - Retry later.')
-							);
-							break;
-						case 502:
-							setValidationOTPError(
-								t('server_unreachable', 'Error 502: Service Unreachable - Retry later.')
-							);
-							break;
-						default:
-							setValidationOTPError(
-								t(
-									'something_went_wrong',
-									'Something went wrong. Please contact your administrator.'
-								)
-							);
-							break;
+				if (res.status === 200) {
+					const authTokenArr = payload?.Body?.AuthResponse?.authToken;
+					const authToken =
+						authTokenArr && authTokenArr.length > 0 ? authTokenArr[0]._content : undefined;
+					if (authToken) {
+						setCookie(ZM_AUTH_TOKEN, authToken);
 					}
-				})
-				.catch((err) => {
-					setValidationOTPError(
-						t('something_went_wrong', 'Something went wrong. Please contact your administrator.')
-					);
-				});
-		},
-		[otpId, otp, trustDevice, t]
-	);
+					setProgress(state.proceedToLoginScreen);
+				} else {
+					setShowValidationCodeError(true);
+				}
+			})
+			.catch((err) => {
+				setShowValidationCodeError(true);
+			});
+	}, [code, username]);
 
 	const onProceedWithSession = useCallback(() => {
-		window.location.assign('/');
+		window.location.assign('/carbonio');
 	}, []);
 
 	return (
@@ -352,24 +152,27 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 							)}
 						</Text>
 					</Row>
-					<Row width="100%">
+					<Row padding={{ bottom: 'medium' }} width="100%">
 						<Input
 							defaultValue={username}
 							data-testid="username"
 							onChange={(e) => {
 								setUsername(e.target.value);
-								setRecoveryEmailError('');
+								setShowRecoveryEmailError(false);
 							}}
 							autocomplete="username"
-							hasError={recoveryEmailError}
+							hasError={showRecoveryEmailError}
 							label={t('username', 'Username')}
 							backgroundColor="gray5"
 						/>
 					</Row>
-					{recoveryEmailError && (
-						<Row mainAlignment="flex-start" width="100%">
-							<Text color="error" size="small" overflow="break-word">
-								{recoveryEmailError}
+					{showRecoveryEmailError && (
+						<Row padding={{ top: 'extrasmall' }} mainAlignment="flex-start">
+							<Text color="error" size="medium" overflow="break-word">
+								{t(
+									'something_went_wrong',
+									'Something went wrong. Please contact your administrator.'
+								)}
 							</Text>
 						</Row>
 					)}
@@ -397,7 +200,7 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 					</Row>
 				</>
 			)}
-			{progress === state.sendRecoveryToken && (
+			{progress === state.sendRecoveryCode && (
 				<>
 					<Row crossAlignment="flex-start" mainAlignment="flex-start" width="100%">
 						<Text overflow="break-word">
@@ -417,10 +220,13 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 							)}
 						</Text>
 					</Row>
-					{sendTokenError && (
-						<Row mainAlignment="flex-start" width="100%">
-							<Text color="error" size="small" overflow="break-word">
-								{sendTokenError}
+					{showSendCodeError && (
+						<Row padding={{ top: 'extrasmall' }} mainAlignment="flex-start">
+							<Text color="error" size="medium" overflow="break-word">
+								{t(
+									'something_went_wrong',
+									'Something went wrong. Please contact your administrator.'
+								)}
 							</Text>
 						</Row>
 					)}
@@ -432,14 +238,14 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 					>
 						<Button
 							data-testid="submit"
-							onClick={onSendRecoveryToken}
+							onClick={onSendRecoveryCode}
 							label={t('send_recovery_code', 'Send Recovery Code')}
 							width="fill"
 						/>
 					</Row>
 				</>
 			)}
-			{progress === state.validateRecoveryToken && (
+			{progress === state.validateRecoveryCode && (
 				<>
 					<Row crossAlignment="flex-start" mainAlignment="flex-start" width="100%">
 						<Text overflow="break-word">
@@ -456,18 +262,21 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 					</Row>
 					<Row padding={{ bottom: 'large' }} width="100%">
 						<Input
-							defaultValue={token}
+							defaultValue={code}
 							data-testid="username"
-							onChange={(e) => setToken(e.target.value)}
+							onChange={(e) => setCode(e.target.value)}
 							autocomplete="code"
 							label={t('code', 'Code')}
 							backgroundColor="gray5"
 						/>
 					</Row>
-					{validationTokenError && (
-						<Row mainAlignment="flex-start" width="100%">
-							<Text color="error" size="small" overflow="break-word">
-								{validationTokenError}
+					{showValidationCodeError && (
+						<Row padding={{ top: 'extrasmall' }} mainAlignment="flex-start">
+							<Text color="error" size="medium" overflow="break-word">
+								{t(
+									'something_went_wrong',
+									'Something went wrong. Please contact your administrator.'
+								)}
 							</Text>
 						</Row>
 					)}
@@ -480,7 +289,7 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 						<Container crossAlignment="flex-start" mainAlignment="flex-start" width="48%">
 							<Button
 								data-testid="resendCode"
-								onClick={onSendRecoveryToken}
+								onClick={onSendRecoveryCode}
 								label={t('resend_Code', 'Resend Code')}
 								type="outlined"
 								size="medium"
@@ -491,11 +300,11 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 						<Container crossAlignment="flex-start" mainAlignment="flex-start" width="48%">
 							<Button
 								data-testid="validateCode"
-								onClick={onValidateToken}
+								onClick={onValidateCode}
 								label={t('validate_code', 'Validate Code')}
 								width="fill"
 								size="medium"
-								disabled={!token}
+								disabled={!code}
 							/>
 						</Container>
 					</Row>
@@ -506,72 +315,6 @@ const ForgetPassword = ({ configuration, disableInputs }) => {
 								'Don’t see the code in your inbox? Wait a couple of minutes or check your spam folder. If you still don’t see it please click on "Resend Code".'
 							)}
 						</Text>
-					</Row>
-				</>
-			)}
-			{progress === state.submitOtpCode && (
-				<>
-					<Row crossAlignment="center" mainAlignment="center" width="100%">
-						<Text size="medium" overflow="break-word">
-							<Trans
-								i18nKey="glad_to_have_you_back_username"
-								defaults="Glad to have you back, <strong>{{ username }}</strong>!"
-								components={{ bold: <strong /> }}
-								values={{ username }}
-							/>
-						</Text>
-					</Row>
-					<Row crossAlignment="center" mainAlignment="center" width="100%">
-						<Text size="medium" overflow="break-word">
-							{t(
-								'we_just_need_one_more_check_before_getting_back_in',
-								'We just need one more check before getting back in.'
-							)}
-						</Text>
-					</Row>
-					<Row crossAlignment="flex-start" mainAlignment="flex-start" width="100%">
-						<Select
-							items={otpList}
-							background="gray5"
-							label={t('choose_otp', 'Choose the OTP Method')}
-							onChange={setOtpId}
-							defaultSelection={otpList[0]}
-						/>
-					</Row>
-					<Row crossAlignment="flex-start" mainAlignment="flex-start" width="100%">
-						<Input
-							defaultValue={otp}
-							hasError={validationOTPError}
-							disabled={disableInputs}
-							onChange={onChangeOtp}
-							label={t('code', 'Code')}
-							backgroundColor="gray5"
-						/>
-					</Row>
-					{validationOTPError && (
-						<Row mainAlignment="flex-start" width="100%">
-							<Text color="error" size="small" overflow="break-word">
-								{validationOTPError}
-							</Text>
-						</Row>
-					)}
-					<Row crossAlignment="center" mainAlignment="flex-start" width="100%">
-						<Row width="65%" mainAlignment="flex-start">
-							<Checkbox
-								value={trustDevice}
-								label={t('rember_this_device', 'Remember this device')}
-								onClick={toggleTrustDevice}
-							/>
-						</Row>
-						<Row width="35%" mainAlignment="flex-end">
-							<Button
-								onClick={submitOtpCb}
-								disabled={!otp}
-								label={t('verify', 'Verify')}
-								width="fill"
-								size="medium"
-							/>
-						</Row>
 					</Row>
 				</>
 			)}
