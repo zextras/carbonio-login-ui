fc426b34a9021f30af1cbabc202bbae45eb03ec7
FrancescoGottardi <gottardifrancesco92@gmail.com>
2025-11-03 15:11:27 +0100

    fix: removed unused code

--- src/components-v1/page-layout.tsx
+++ src/components-v1/page-layout.tsx
@@ -131,7 +131,7 @@ export default function PageLayout({
 	type logoType = {
 		image: string;
 		width: string;
-		url?: string | undefined;
+		url?: string;
 	};
 
 	const [logo, setLogo] = useState<logoType>();
